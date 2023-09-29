6787be928788ea3e8de47c65a369bcca554be14e
foullanifatimazohra <foullanifatoum@gmail.com>
2023-09-29 19:54:14 +0100

    feat : add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,3 @@
-import Card from "@/components/pages/home/Card";
-import Button from "@/components/ui/Button";
-import Text from "@/components/ui/Text";
-
 export default function Home() {
-  return (
-    <div className="container mx-auto">
-      <h1>First page</h1>
-      <Button type="primary" title="Get Access" />
-      <Button type="secondary" title="Request Access" />
-      <Text
-        content="Unparalleled personalized outreach and optimized customer engagement"
-        type="h1"
-      />
-      <div className="w-1/2">
-        <Card rounded={true} padding="p-12" bgColor="bg-[#E6F8FA]">
-          <Text content="Supercharge your Outbound Sales" type="h2" />
-          <div>
-            <Text
-              content="Unleash personalized outreach that forges meaningful connections and creates effortless leads. "
-              type="p"
-            />
-            <Text
-              content="Salient is your AI sales development representative that;"
-              type="p"
-            />
-            <ul className="list-disc ml-5">
-              <li>Generates human-quality outbound in your tone of voice</li>
-              <li>Proactively sets up meetings</li>
-              <li>Responds to common queries</li>
-            </ul>
-          </div>
-        </Card>
-      </div>
-    </div>
-  );
+  return <div className="container mx-auto"></div>;
 }
--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Image from "next/image";
+import Logo from "../../public/svg/LogoSalient.svg";
+import Button from "../ui/Button";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="h-10 py-5 container mx-auto">
+      <div className="flex justify-between">
+        <Image src={Logo} alt="Logo " height={48} width={120} />
+        <nav>
+          <ul className="flex items-center gap-5">
+            <li>Who are we?</li>
+            <li>Contact Us</li>
+            <Button type="secondary" title="Request Access" />
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
